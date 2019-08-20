8093ac919aaa425d85f71d771dfe72523edc1418
muFenJun <1040475997@qq.com>
2019-08-20 19:27:48 +0800

    小三修改代码

--- ee100/HelloProjet/src/Main.java
+++ ee100/HelloProjet/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("老子写的代码!!!");
 		System.out.println("小李四写的代码!!!");
+		System.out.println("小三写的代码!!!");
     }
 }
