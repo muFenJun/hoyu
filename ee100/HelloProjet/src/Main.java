82c64e34914f97c3a5f6d09e97885821d5af370c
xnzhangpan6 <zhangpan103@jd.com>
2019-11-19 10:01:31 +0800

    tianjia

--- ee100/HelloProjet/src/Main.java
+++ ee100/HelloProjet/src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 		System.out.println("小无写的代码!!!");
 		System.out.println("小小小六写的代码!!!");
 		System.out.println("小小小八写的代码!!!");
+		System.out.println("jingdong");
 		System.out.println("Hello!!!");
 		int i = 9;
 		System.out.println(i);
