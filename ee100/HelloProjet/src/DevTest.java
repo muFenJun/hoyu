33ccb2f8f488412dc50c3fb39055f96c6847fe30
xnzhangpan6 <zhangpan103@jd.com>
2019-11-19 10:40:58 +0800

    修改

--- ee100/HelloProjet/src/DevTest.java
+++ ee100/HelloProjet/src/DevTest.java
@@ -5,8 +5,9 @@
  */
 public class DevTest {
     public static void main(String[] args) {
-        System.out.println("dev分支");
-        System.out.println("dev分支");
+        System.out.println("dev分支1");
+        System.out.println("dev分支2");
+        System.out.println("dev分支3");
 
     }
 }
