5fedf70a4b36a2f3dcbad7535f2882c475a2f86e
xnzhangpan6 <zhangpan103@jd.com>
2019-11-19 11:05:37 +0800

    1111

--- ee100/HelloProjet/src/DevTest.java
+++ ee100/HelloProjet/src/DevTest.java
@@ -14,4 +14,26 @@ public class DevTest {
         System.out.println("dev分支6");
 
     }
+
+    private int test232I(){
+        System.out.println("dev分支1");
+        System.out.println("dev分支2");
+        System.out.println("dev分支3");
+        System.out.println("dev分支7");
+        System.out.println("dev分支8et");
+        System.out.println("dev分支5");
+        System.out.println("dev分支6");
+        return 1;
+    }
+
+    private int testIflkjakldjf(){
+        System.out.println("dev分fsaf支1");
+        System.out.println("dev分支2");
+        System.out.println("dedsv分支3");
+        System.out.println("devadsf分支7");
+        System.out.println("devadf分支8et");
+        System.out.println("dev分支5");
+        System.out.println("dev分dsf支6");
+        return 1;
+    }
 }
