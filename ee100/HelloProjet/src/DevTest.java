b6be35a3d7771520a7ea910684251743304a4cfc
xnzhangpan6 <zhangpan103@jd.com>
2019-11-19 11:03:43 +0800

    1111

--- ee100/HelloProjet/src/DevTest.java
+++ ee100/HelloProjet/src/DevTest.java
@@ -12,4 +12,27 @@ public class DevTest {
         System.out.println("dev分支6");
 
     }
+
+    private int testII(){
+        System.out.println("dev分支1");
+        System.out.println("dev分支2");
+        System.out.println("dev分支3");
+        System.out.println("dev分支7");
+        System.out.println("dev分支8et");
+        System.out.println("dev分支5");
+        System.out.println("dev分支6");
+        return 1;
+    }
+
+    private int testI(){
+        System.out.println("dev分支11");
+        System.out.println("dev分支12");
+        System.out.println("dev分支13");
+        System.out.println("dev分支17");
+        System.out.println("dev分支18et");
+        System.out.println("dev分支51");
+        System.out.println("dev分支16");
+        return 1;
+    }
+
 }
