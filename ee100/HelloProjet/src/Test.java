6dfde123ebb3f911e45e589ad73c2fbe94a83e55
xnzhangpan6 <zhangpan103@jd.com>
2019-11-19 10:01:48 +0800

    tianjia

---
+++ ee100/HelloProjet/src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * - @Description:
+ * - @Author:  zhangpan
+ * - @Date:  2019/11/19 9:57
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello jingdong");
+    }
+}
